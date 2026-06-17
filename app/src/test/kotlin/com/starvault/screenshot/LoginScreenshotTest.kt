b0ddd211815fd911050026444918be838639383b
panyongjian <pyongjian1992@gmail.com>
2026-06-17 20:14:11 +0800

    test(cloud115): regenerate LoginScreen Paparazzi snapshots for real QR shape

--- app/src/test/kotlin/com/starvault/screenshot/LoginScreenshotTest.kt
+++ app/src/test/kotlin/com/starvault/screenshot/LoginScreenshotTest.kt
@@ -2,6 +2,7 @@ package com.starvault.screenshot
 
 import app.cash.paparazzi.Paparazzi
 import com.android.ide.common.rendering.api.SessionParams
+import androidx.compose.ui.graphics.asImageBitmap
 import com.starvault.theme.StarVaultTheme
 import com.starvault.ui.login.LoginScreen
 import com.starvault.ui.login.LoginUiState
@@ -26,9 +27,14 @@ class LoginScreenshotTest {
     )
 
     @Test fun login_waiting() = paparazzi.snapshot {
+        // 真 QR：mock 一个 1x1 蓝色 Bitmap，Compose Image 会用 ContentScale.Fit 放大到 220dp
+        val androidBitmap = android.graphics.Bitmap.createBitmap(
+            intArrayOf(0xFF2F6FEB.toInt()), 1, 1, android.graphics.Bitmap.Config.ARGB_8888
+        )
+        val bmp = androidBitmap.asImageBitmap()
         StarVaultTheme {
             LoginScreen(
-                state = LoginUiState.Waiting(expireSeconds = 120),
+                state = LoginUiState.Waiting(qrBitmap = bmp, expireSeconds = 120),
                 onRefresh = {},
             )
         }
